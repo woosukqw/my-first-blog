6869ab33720864eabb317bf38b23b7c33333dd1e
woosukqw <woosukqw@naver.com>
2019-10-11 01:26:46 +0900

    first commit

---
+++ venv01/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/woosuk/djangoProjects/mysite_fix/mysite_jiho/venv01/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
